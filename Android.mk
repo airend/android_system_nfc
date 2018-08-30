abc2d01ffad90d6bfcb17c1f8acee49967503db4
D. Andrei Măceș <andrei@unlegacy-android.org>
2018-08-30 11:06:07 +0300

    bcm2079x: Switch back to more flexible Android.mk
    Redone based on last Android.mk, and all the Android.bp changes since.
    Stub Android.bp needs to be kept for subdirs = ["src"] lookup.
    
    Change-Id: Ie90d3fa81da354edc4c2de084573b80615781668
    Signed-off-by: D. Andrei Măceș <andrei@unlegacy-android.org>

---
+++ Android.mk
@@ -0,0 +1,43 @@
+LOCAL_PATH:= $(call my-dir)
+NFA := src/nfa
+NFC := src/nfc
+HAL := src/hal
+UDRV := src/udrv
+HALIMPL := halimpl/bcm2079x
+D_CFLAGS := -DBUILDCFG=1 \
+    -Wno-deprecated-register \
+    -Wno-unused-parameter \
+    -Wno-missing-field-initializers \
+
+
+######################################
+# Build shared library system/lib/hw/nfc_nci.*.so for Hardware Abstraction Layer.
+# Android's generic HAL (libhardware.so) dynamically loads this shared library.
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := nfc_nci.bcm2079x.default
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_SRC_FILES := $(call all-c-files-under, $(HALIMPL)) \
+    $(call all-cpp-files-under, $(HALIMPL)) \
+    src/adaptation/CrcChecksum.cpp \
+    src/nfca_version.c
+LOCAL_SHARED_LIBRARIES := libcutils liblog libhwbinder
+LOCAL_HEADER_LIBRARIES := libhardware_headers libhardware_legacy_headers
+LOCAL_C_INCLUDES := \
+    $(LOCAL_PATH)/$(HALIMPL)/include \
+    $(LOCAL_PATH)/$(HALIMPL)/gki/ulinux \
+    $(LOCAL_PATH)/$(HALIMPL)/gki/common \
+    $(LOCAL_PATH)/$(HALIMPL)/adaptation \
+    $(LOCAL_PATH)/$(HAL)/include \
+    $(LOCAL_PATH)/$(HAL)/int \
+    $(LOCAL_PATH)/$(NFC)/include \
+    $(LOCAL_PATH)/$(NFA)/include \
+    $(LOCAL_PATH)/$(UDRV)/include \
+    $(LOCAL_PATH)/src/include
+LOCAL_CFLAGS := $(D_CFLAGS) -DNFC_HAL_TARGET=TRUE -DNFC_RW_ONLY=TRUE
+include $(BUILD_SHARED_LIBRARY)
+
+
+######################################
+include $(call all-makefiles-under,$(LOCAL_PATH))
